ad7a650e38244cdddc33fd9b17f501e6ad70cc28
Manjeet Saini <manjeetsaini2003.com@gmail.com>
2026-05-29 11:04:00 +0530

    Homepage desgined-01

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-   <>
+   <div className="bg-black">
    This is my new Project --- Image Editor
-   </>
+   <Button variant = "primary">Submit</Button>
+   <Button variant = "glass">Submit</Button>
+    </div>
   );
 }
